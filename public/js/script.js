dc86adb13ac3a5ab58408c68d56e620e199989b9
erlopez11 <lopezelainy@gmail.com>
2025-03-08 16:38:36 -0600

    wip: add event handleer for mobile nav toggle

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primaryNav');
+
+navToggle.addEventListener('click', () => {
+    const visible = primaryNav.getAttribute('data-visible');
+
+    if (visible === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false)
+    }
+});
